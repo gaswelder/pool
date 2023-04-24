07b2f2c476be54cce346000179e533cf5d2fa7d2
gasweld <gasweld@gmail.com>
2023-04-24 22:12:23 +0300

    Switch the database to text files

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,12 +1,53 @@
 import * as fs from "fs";
 import { WorkoutFromJSON } from "../types";
 
-export const loadWorkouts = () => table("data");
+export const loadWorkouts = () => read(); // table("data");
 export const saveWorkouts = (ww: WorkoutFromJSON[]) => writeTable("data", ww);
 
 const table = (name: string) => {
   const data = JSON.parse(fs.readFileSync(`data/${name}.json`).toString());
   return data.records as WorkoutFromJSON[];
+};
+
+const read = () => {
+  const ww = [] as WorkoutFromJSON[];
+  const lines = fs.readFileSync(`data/workouts.txt`).toString().split(/\n+/);
+  const r = (prefix: string) => {
+    const line = lines.shift();
+    if (!line) {
+      throw new Error(`expected ${prefix}, got end of lines`);
+    }
+    if (!line.startsWith(prefix)) {
+      throw new Error(`expected ${prefix}, got ${line}`);
+    }
+    return line.substring(prefix.length, line.length);
+  };
+  const rval = (key: string) => r(`${key}: `);
+  const rtitle = () => r("## ");
+  while (lines.length > 0) {
+    const title = rtitle();
+    const id = rval("id");
+    const created = rval("created");
+    const swam = rval("swam");
+    const archived = rval("archived");
+    const w = {
+      title,
+      id,
+      created,
+      swam,
+      archived,
+      ex: [] as string[],
+    };
+    while (lines.length > 0 && !lines[0].startsWith("##")) {
+      const line = lines.shift();
+      if (!line) {
+        continue;
+      }
+      w.ex.push(line);
+    }
+    ww.push(w);
+  }
+  return ww;
 };
 
 const writeTable = (name: string, records: WorkoutFromJSON[]) => {
@@ -15,6 +56,22 @@ const writeTable = (name: string, records: WorkoutFromJSON[]) => {
     `data/backup-${Date.now()}-${name}.json`
   );
   fs.writeFileSync(`data/${name}.json`, JSON.stringify({ records }, null, 2));
+  const s = records
+    .flatMap((w) => {
+      const lines = [`## ${w.title}\n`];
+      lines.push(`id: ${w.id}`);
+      lines.push(`created: ${w.created}`);
+      lines.push(`swam: ${w.swam || ""}`);
+      lines.push(`archived: ${w.archived || ""}`);
+      lines.push("");
+      for (const e of w.ex) {
+        lines.push(e);
+      }
+      lines.push("\n");
+      return lines;
+    })
+    .join("\n");
+  fs.writeFileSync(`data/workouts.txt`, s);
 };
 
 export const loadFavs = () => {
