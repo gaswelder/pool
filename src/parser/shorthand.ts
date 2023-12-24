9690406ad25978ef98ce923daaf5c249050b22f4
gasweld <gasweld@gmail.com>
2023-12-24 18:16:11 +0200

    Inline pbuf

--- src/parser/shorthand.ts
+++ src/parser/shorthand.ts
@@ -1,5 +1,3 @@
-import { PBuf, pbuf } from "./pbuf";
-
 export type ESet = ReturnType<typeof parseSet>;
 export type Line = ReturnType<typeof parseLine>;
 
@@ -98,5 +96,69 @@ const parseDescription = (buf: PBuf) => {
   return {
     desc: noneqs.join(" ").trim(),
     equipment: eqs,
+  };
+};
+
+type PBuf = ReturnType<typeof pbuf>;
+
+const pbuf = (s: string) => {
+  let pos = 0;
+  const peek = () => {
+    if (pos >= s.length) {
+      return null;
+    }
+    return s[pos];
+  };
+  const get = () => {
+    if (pos >= s.length) {
+      throw new Error(`EOF`);
+    }
+    return s[pos++];
+  };
+  const isdigit = (x: null | string) => x && x.match(/\d/);
+  const isspace = (x: null | string) => x && x.match(/\s/);
+  const is = (x: null | string, re: RegExp) => x && x.match(re);
+  const spaces = () => {
+    while (isspace(peek())) {
+      get();
+    }
+  };
+  return {
+    get,
+    number() {
+      spaces();
+      let r = "";
+      while (peek() !== null && isdigit(peek())) {
+        r += get();
+      }
+      if (peek() == ".") {
+        r += get();
+        while (peek() !== null && isdigit(peek())) {
+          r += get();
+        }
+      }
+      return r;
+    },
+    times() {
+      spaces();
+      if (peek() == "x" || peek() == "×") {
+        const r = get();
+        spaces();
+        return r;
+      }
+      return "";
+    },
+    word() {
+      let r = "";
+      while (peek() !== null && !isspace(peek())) {
+        r += get();
+      }
+      return r;
+    },
+    peek,
+    rest() {
+      return s.substring(pos, s.length);
+    },
+    spaces,
   };
 };
