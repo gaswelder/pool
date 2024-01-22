a7484dfe01e56e51945fcc6bda91db61dc2ff978
gasweld <gasweld@gmail.com>
2024-01-22 20:40:08 +0200

    Add summaries

--- src/frontend/Draft.tsx
+++ src/frontend/Draft.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from "react";
 import styled from "styled-components";
 import { Collapsible } from "./lib/Collapsible";
 import { WorkoutTable } from "./WorkoutTable";
-import { parseDraft } from "./draft";
+import { SetProg, parseDraft } from "./draft";
 import { Theme } from "./theme";
 
 const DraftTextarea = styled.textarea`
@@ -24,6 +24,9 @@ const WorkoutContainer = styled.div`
     padding: ${Theme.h} ${Theme.w};
     margin-right: 6px;
     margin-top: 6px;
+  }
+  & section h3 + p {
+    font-size: 90%;
   }
   @media print {
     & section {
@@ -68,10 +71,32 @@ export const Draft = ({ initialText }: { initialText: string }) => {
           prog.days.map((day) => (
             <section key={day.name}>
               <h3>{day.name}</h3>
+              <p>{summary(day.sets)}</p>
               <WorkoutTable workout={day.sets} />
             </section>
           ))}
       </WorkoutContainer>
     </>
   );
+};
+
+const summary = (sets: SetProg[]) => {
+  const firstWord = (s: string) => s.split(" ")[0];
+  const flines = sets.flatMap((x) => x.lines);
+
+  const bar = {} as Record<string, number>;
+  let total = 0;
+  flines.forEach((x) => {
+    total += x.repeats * x.amount;
+    const kind = firstWord(x.desc);
+    bar[kind] = (bar[kind] || 0) + x.repeats * x.amount;
+  });
+
+  return Object.entries(bar)
+    .sort((a, b) => b[1] - a[1])
+    .filter((e) => e[1] > 0)
+    .map(([kind, sum]) => {
+      return `${Math.round((100 * sum) / total)}% ${kind}`;
+    })
+    .join(", ");
 };
