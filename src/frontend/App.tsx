45ec7e1159208a153674583c7f01dbf7cb92d674
gasweld <gasweld@gmail.com>
2024-02-21 23:07:50 +0200

    Add an interval pace calculator

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useLocation } from "./lib/A";
 import { Draft } from "./Draft";
 import { Theme } from "./theme";
+import { Collapsible } from "./lib/Collapsible";
 
 const AppDiv = styled.div`
   background: #d1dfec;
@@ -102,10 +103,52 @@ const Content = () => {
           <button type="button" disabled={!initialized} onClick={init}>
             Generate
           </button>
+          <Collapsible
+            header={() => <>Calculator</>}
+            content={() => <Calc />}
+          />
           <Draft key={ver} initialText={text} />
         </>
       );
     default:
       return <>unknown location: {location.pathname}</>;
   }
+};
+
+const Calc = () => {
+  const [length, setLength] = useState(40);
+  const [interval, setInterval] = useState("1:20");
+  const pace = formatTime((parseTime(interval) / length) * 100);
+  return (
+    <>
+      <label>
+        Length (m):{" "}
+        <input
+          type="number"
+          size={2}
+          value={length}
+          onChange={(e) => setLength(parseInt(e.target.value, 10))}
+        />
+      </label>
+      <label>
+        Interval (m:s):{" "}
+        <input
+          size={4}
+          value={interval}
+          onChange={(e) => setInterval(e.target.value)}
+        />
+      </label>
+      pace: {pace}
+    </>
+  );
+};
+
+const parseTime = (time: string) => {
+  const [m, s] = time.split(":").map((x) => parseInt(x, 10));
+  return m * 60 + s;
+};
+const formatTime = (s: number) => {
+  const sec = s % 60;
+  const m = Math.floor(s / 60);
+  return m + ":" + sec.toString().padStart(2, "0");
 };
