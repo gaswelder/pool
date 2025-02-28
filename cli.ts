5b5a09abf7a42de19adf233f8c6b96546990d9cd
gasweld <gasweld@gmail.com>
2025-02-28 19:37:57 +0200

    cli: cleaning

--- cli.ts
+++ cli.ts
@@ -30,6 +30,22 @@ const gensst = () => {
   }
 };
 
+/**
+ * Returns item's last time as a timestamp.
+ */
+const lastTime = (x: Item) => {
+  const ts = (s: string) => new Date(s).getTime();
+  const n = x.history.length;
+  if (n == 0) {
+    return ts("2000-01-01");
+  }
+  const m = x.history[n - 1].match(/(\d\d\d\d\-\d\d\-\d\d)/);
+  if (!m) {
+    return ts("2000-01-01");
+  }
+  return ts(m[1]);
+};
+
 const cmds = [
   {
     name: "themes",
@@ -59,7 +75,11 @@ const cmds = [
           include.push(arg);
         }
       }
-      const ss = parseSuperset()
+
+      const now = Date.now();
+
+      parseSuperset()
+        // Select exercises that match the filter.
         .filter((x) => {
           if (include.length > 0 && !include.includes(x.kind)) return false;
           if (exclude.length > 0 && exclude.includes(x.kind)) return false;
@@ -68,29 +88,20 @@ const cmds = [
         .filter((x) => {
           if (x.categories.includes("50")) return false;
           return true;
-        });
-
-      // Return item's last time as a timestamp.
-      const lastTime = (x: Item) => {
-        const ts = (s: string) => new Date(s).getTime();
-        if (x.history.length == 0) return ts("2000-01-01");
-        const m = x.history[x.history.length - 1].match(
-          /(\d\d\d\d\-\d\d\-\d\d)/
-        );
-        if (!m) return ts("2000-01-01");
-        return ts(m[1]);
-      };
-
-      const now = Date.now();
-      const rows = ss.map((s) => {
-        let gap = now - lastTime(s);
-        // Exercises marked as r=2 will be scheduled twice as often.
-        if (s.categories.includes("r=2")) {
-          gap *= 2;
-        }
-        return { s, gap };
-      });
-      rows
+        })
+        // Add priority to each match based on how long ago it was
+        // logged last time.
+        .map((s) => {
+          let gap = now - lastTime(s);
+          // Exercises marked as r=2 will be scheduled twice as often.
+          // This would work for an arbitrary factor, not just 2, but only 2
+          // was needed so far.
+          if (s.categories.includes("r=2")) {
+            gap *= 2;
+          }
+          return { s, gap };
+        })
+        // Sort by the calculated priority.
         .sort((a, b) => {
           if (a.gap != b.gap) return b.gap - a.gap;
           return Math.random() - 0.5;
